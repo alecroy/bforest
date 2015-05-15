d5df4ea29cd281ae631707c902c76003faed23e1
Alec Hollingsworth <alec@mailc.net>
2015-05-14 21:23:17 -0700

    add tests for .prepend(..) and list-like .toString(..)

--- test/bforest_test.js
+++ test/bforest_test.js
@@ -21,4 +21,23 @@ describe('the BForest data structure', function() {
   it('empty.tail() should be empty', function() {
     expect(new BForest().tail().isEmpty()).to.be.true;
   });
+
+  describe('prints just like a list', function() {
+    it('empty is []', function() {
+      expect(new BForest().toString()).to.equal('[]');
+    });
+
+    var oneTwoThree = new BForest().prepend([1, 2, 3]);
+    it('prepending [1, 2, 3] to [] should make [1, 2, 3]', function() {
+      expect(oneTwoThree.toString()).to.equal('[1, 2, 3]');
+    });
+
+    it('the head of [1, 2, 3] should be 1', function() {
+      expect(oneTwoThree.head()).to.equal(1);
+    });
+
+    it('the tail of [1, 2, 3] should be [2, 3]', function() {
+      expect(oneTwoThree.tail().toString()).to.equal('[2, 3]');
+    });
+  });
 });
